1d357a4da34dcbdb8e225ca6ff57cb888f2d4aec
Ananta Vigave <104813532+Ananta2003@users.noreply.github.com>
2024-10-04 22:50:44 +0530

    Update src/selector.js
    Co-authored-by: Eduard-Constantin Ibinceanu <ibinceanu.eduard@yahoo.com>

--- src/selector.js
+++ src/selector.js
@@ -63,7 +63,7 @@ export const fileSelector = createPrompt((config, done) => {
     });
 
   // Add navigation options
-  const goBackOption = { name: `\x1b[90m[..]  BACK\x1b[0m`, value: "..", isDirectory: true };
+  const goBackOption = { name: `\x1b[90m[..]  BACK\x1b[0m`, value: "..", isDirectory: false};
   const exitOption = { name: `\x1b[91mⓧ  EXIT\x1b[0m`, value: null, isDirectory: false };
   const submitOption = { name: `\x1b[92m✔  SUBMIT SELECTED FILES\x1b[0m`, value: "submit", isDirectory: false };
 
